ff111e39873d584f0ea6e1cb0838ef42e45e47a8
bandess <andras.karpati@appo.io>
2020-06-22 14:57:40 +0200

    pricebreakdown support

---
+++ src/Response/PriceBreakdown.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace GoGlobal\Response;
+
+use GoGlobal\ResponseAbstract;
+use GoGlobal\ResponseInterface;
+
+class PriceBreakdown extends ResponseAbstract implements ResponseInterface
+{
+    protected function process() {
+
+    }
+}
